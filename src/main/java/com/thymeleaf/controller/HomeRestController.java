78ed2de25fc34c4521bf098479cffea259043e7c
WilliamKXiong <mana_grey@hotmail.com>
2025-02-04 23:59:47 -0600

    Correct the home api mapping

--- src/main/java/com/thymeleaf/controller/HomeRestController.java
+++ src/main/java/com/thymeleaf/controller/HomeRestController.java
@@ -4,14 +4,16 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/")
 public class HomeRestController {
     @Value("${application.version}")
     private String applicationVersion;
 
-    @GetMapping({"", "/"})
+    @GetMapping({""})
     public String index() {
         return "Current Version: " + getApplicationVersion();
     }
